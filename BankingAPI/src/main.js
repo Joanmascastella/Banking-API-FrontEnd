9f2845967d2683bfa1aea6464c6fb05b43b9dbe7
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-04-19 13:06:48 +0200

    boilerplate

---
+++ BankingAPI/src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import './assets/main.css';
+
+const $axios = axios.create({
+  baseURL: 'http://localhost/'
+});
+
+//Making authtoken globally accessible
+$axios.interceptors.request.use(function (config) {
+  const authToken = localStorage.getItem('authToken');
+  if (authToken) {
+      config.headers.Authorization = `Bearer ${authToken}`;
+  }
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(router);
+app.use(pinia);
+app.config.globalProperties.$axios = $axios;
+
+app.mount('#app');
---
+++ BankingAPI/src/stores/auth.js
@@ -0,0 +1,62 @@
+import { defineStore } from 'pinia';
+
+function decodeToken(token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    token: null,
+    refreshToken: null
+  }),
+  actions: {
+    setUser(userData) {
+      this.user = {
+        id: userData.id,
+        username: userData.name,
+        email: userData.email,
+        role: userData.role,
+      };
+      this.token = userData.authToken; 
+      this.refreshToken = userData.refreshToken; 
+      localStorage.setItem('authToken', userData.authToken);
+      localStorage.setItem('refreshToken', userData.refreshToken);
+    },
+
+    logout() {
+      this.user = null;
+      this.token = null;
+      this.refreshToken = null;
+      localStorage.removeItem('authToken');
+      localStorage.removeItem('refreshToken');
+    },
+    checkUser() {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        this.token = token;
+        try {
+          const decoded = decodeToken(token);
+          this.user = {
+            id: decoded.data.id,
+            username: decoded.data.username,
+            role: decoded.data.role,
+          };
+        } catch (error) {
+          console.error("Error decoding token:", error);
+        }
+      }
+    },
+  },
+  getters: {
+    isAdmin: (state) => state.user?.role === 'admin',
+    isUser: (state) => state.user?.role === 'user',
+    userId: (state) => state.user?.id,
+  },
+});
