31c95bff6a0d4d0497088a3fa4f3dab8dd3a71c9
Elian Borcut <77327525+elianborcut16@users.noreply.github.com>
2024-05-16 17:29:18 +0200

    a

--- src/components/common/atm/dashboard/VerifiedViewATM.vue
+++ src/components/common/atm/dashboard/VerifiedViewATM.vue
@@ -3,15 +3,21 @@
       <h2>ATM Dashboard</h2>
       <div class="grid">
         <div>
-          <img src="../../assets/img/withdraw.png" alt="Withdraw">
+          <img src="../../../../assets/img/withdraw.png" alt="Withdraw">
           <button @click="goToWithdraw">Withdraw</button>
         </div>
         <div>
-          <img src="../../assets/img/deposit.png" alt="Deposit">
+          <img src="../../../../assets/img/deposit.png" alt="Deposit">
           <button @click="goToDeposit">Deposit</button>
         </div>
       </div>
     </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </template>
   <script>
   export default {
@@ -31,47 +37,33 @@
       }
     }
   }
-  </script>
-  
-  <style scoped>
-  h2 {
-    margin-top: 20vh;
-    text-align: center;
-  }
-  
-  .content {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
-  
-  .grid {
-    width: 70%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 20px;
-  }
-  
-  img {
-    border: solid;
-    border-bottom: none;
-    border-color: orange;
-    padding: 5px;
-    width: 100%;
-  }
-  
-  button {
-    width: 100%;
-    color: #0BCA48;
-    background-color: #055A0E;
-    padding: 10px;
-    border: none;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #0BCA48;
-    color: white;
-  }
-  </style>
+</script>
+
+<style scoped>
+h2 {margin-top: 20vh;
+text-align:center;}
+
+.content {
+min-height:100vh;
+display:flex;
+align-items:center;
+flex-direction:column;
+
+}
+
+.grid {
+  width:70%;
+}
+
+
+img {border: solid;
+border-bottom:none;
+border-color:orange;
+padding: 5px;
+width:100%;
+}
+
+button {width: 100%;
+color:#0BCA48;
+background-color:#055A0E;}
+</style>
