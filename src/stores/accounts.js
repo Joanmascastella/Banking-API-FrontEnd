c6bafdcb1b58d10c91251331fe4b5725f09dcf46
MariaRaluca Diaconescu <160227814+IumaHedgehogie@users.noreply.github.com>
2024-05-25 12:46:58 +0200

    Changed how data is sent to frontend

--- src/stores/accounts.js
+++ src/stores/accounts.js
@@ -1,43 +1,53 @@
 import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 
 
+export const accounts = defineStore('accounts', () => {
 
-export const accounts = defineStore('accounts', {
-    state() {
-        return {
-        };
-    },
+    const accounts = ref([])
+    const accountsWithLimit = ref([])
+    const errorMessage = ref(null)
+    const getAccounts = computed(() => accounts.value.data)
+    const getAccountsWithLimit = computed(() => accountsWithLimit.value.data)
 
-    actions: {
 
-        async retrieveAllAccounts() {
-            try {
-                return await this.$axios.get('/accounts/customers');
-            }
-            catch (error) {
-                const errorMessage = error.response.status;
-                return { error: errorMessage };
-            }
-        },
-        async retrieveAccountsByAbsoluteLimit(absoluteLimit) {
-            try {
-                return await this.$axios.get('/accounts/byAbsoluteLimit?absoluteLimit=' + absoluteLimit);
-            }
-            catch (error) {
-                const errorMessage = error.response.status;
-                return { error: errorMessage };
-            }
-        },
-        async updateAccountLimit(account) {
-            try {
-                const response = await this.$axios.put(`/accounts/customers`, account);
-                return { success: true };
-            } catch (error) {
-                return { success: false, message: error.message || "Error updating the absolute transaction limit." };
-            }
-        },
-
+    async function retrieveAllAccounts() {
+        try {
+            accounts.value = await this.$axios.get('/accounts/customers');
+            return accounts.value
+        }
+        catch (error) {
+            errorMessage.value = error.response.status;
+        }
     }
 
+    async function retrieveAccountsByAbsoluteLimit(absoluteLimit) {
+        try {
+            accountsWithLimit.value = await this.$axios.get('/accounts/byAbsoluteLimit?absoluteLimit=' + absoluteLimit);
+        }
+        catch (error) {
+            errorMessage.value = error.response.status;
+        }
+    }
+
+    async function updateAccountLimit(account) {
+        try {
+            const response = await this.$axios.put(`/accounts/customers`, account);
+            return { success: true };
+        } catch (error) {
+            return { success: false, message: error.message || "Error updating the absolute transaction limit." };
+        }
+    }
+
+    return {
+        retrieveAllAccounts,
+        retrieveAccountsByAbsoluteLimit,
+        updateAccountLimit,
+        errorMessage,
+        accounts,
+        getAccounts,
+        getAccountsWithLimit,
+    }
 
 });
+
